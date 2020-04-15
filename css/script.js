8b6b58805130f3cae1ca4d6e78e2e8d2a05c1930
pmarti99 <parkerjmartin883@gmail.com>
2020-04-14 21:24:08 -0600

    javascript update

--- css/script.js
+++ css/script.js
@@ -1,27 +1,39 @@
 $(document).ready(function() {
     let teamIDs = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
-            // function getTeam(teamID) {
-            //     let queryURL = 'https://www.balldontlie.io/api/v1/teams/' + teamID;
-            //     $.ajax({
-            //         url: queryURL,
-            //         method: "GET"
-            //     }).then(function (response) {
-            //         console.log(response.full_name);
-            //         let option = $('<option>');
-            //         option.text(response.full_name);
-            //         $('#teamselect').append(option);
+            function getTeam(teamID) {
+                let queryURL = 'https://www.balldontlie.io/api/v1/teams/' + teamID;
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response);
+                    // let option = $('<option>');
+                    // option.text(response.full_name);
+                    // $('#teamselect').append(option);
 
-            //         console.log(queryURL);
-            //     }).catch(function (error) {
-            //         console.log(error);
+                    console.log(queryURL);
+                }).catch(function (error) {
+                    console.log(error);
                     
-            //     });
-            // }
-            // for (let i = 0; i < teamIDs.length; i++) {
-            //     let teamID=teamIDs[i];
-            //     getTeam(teamID);
+                });
+            }
+            for (let i = 0; i < teamIDs.length; i++) {
+                let teamID=teamIDs[i];
+                getTeam(teamID);
 
-            // }
+            };
+        //     function sportsDB(){
+        //     let teamURL = 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387';
+        //     $.ajax({
+        //         url: teamURL,
+        //         method: "GET"
+        //     }).then(function (response) {
+        //         for(let i=0; i<response.teams.length; i++){
+        //         console.log(response.teams[i].strTeamLogo);
+        //         sportsDB();
+        //     }
+        //     })
+        // };
             
             let queryURL = 'https://www.balldontlie.io/api/v1/teams/';
             $.ajax({
@@ -34,8 +46,6 @@ $(document).ready(function() {
                     let option = $('<option>');
                     option.text(team.full_name);
                     $('#teamselect').append(option);
-                    
-
                 };
                 // console.log(response.full_name);
                 // let option = $('<option>');
